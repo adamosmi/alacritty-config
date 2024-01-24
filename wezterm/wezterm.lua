e604f58f5ada39c512fcfc21e4670777eb9c9810
Andrew Damon-Smith <andrewdamonsmith@gmail.com>
2024-01-24 12:39:21 -0500

    Removed borders and added default window size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,10 @@ local config = {}
 
 config.color_scheme = 'catppuccin-mocha'
 config.hide_tab_bar_if_only_one_tab = true
+-- Use `SUPER` key to move around window and `ALT+ENTER` to go into full screen
+config.window_decorations = "NONE"
 config.font = wezterm.font("Iosevka Nerd Font Mono", {weight="Regular", stretch="Normal", style="Normal"})
+config.initial_cols = 120
+config.initial_rows = 24
 
 return config
