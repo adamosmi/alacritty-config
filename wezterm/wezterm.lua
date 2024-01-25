38192713fbd7501ef5163afd1d89f6ea61dd3a11
Andrew Damon-Smith <andrewdamonsmith@gmail.com>
2024-01-24 23:39:59 -0500

    Added documentation

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,18 @@
 local wezterm = require 'wezterm'
 local config = {}
 
+-- Install color scheme
 config.color_scheme = 'catppuccin-mocha'
+
+-- Make terminal window borderless (NOTE: Use `SUPER` key to move around window and `ALT+ENTER` to go into full screen)
 config.hide_tab_bar_if_only_one_tab = true
--- Use `SUPER` key to move around window and `ALT+ENTER` to go into full screen
 config.window_decorations = "NONE"
-config.font = wezterm.font("Iosevka Nerd Font Mono", {weight="Regular", stretch="Normal", style="Normal"})
+
+-- Resize window on startup
 config.initial_cols = 120
 config.initial_rows = 24
+
+-- Install use font
+config.font = wezterm.font("Iosevka Nerd Font Mono", {weight="Regular", stretch="Normal", style="Normal"})
 
 return config
